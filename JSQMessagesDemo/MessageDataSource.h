29a3ce89a0d2276527fcebaf93035b5449a9e6b7
gjwang <guijiewan@gmail.com>
2015-06-05 18:47:20 +0800

    customized datasource of chat UI

---
+++ JSQMessagesDemo/MessageDataSource.h
@@ -0,0 +1,13 @@
+//
+//  MessageDataSource.h
+//  JSQMessages
+//
+//  Created by gjwang on 6/5/15.
+//  Copyright (c) 2015 Hexed Bits. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MessageDataSource : NSObject
+
+@end
---
+++ JSQMessagesDemo/MessageDataSource.m
@@ -0,0 +1,13 @@
+//
+//  MessageDataSource.m
+//  JSQMessages
+//
+//  Created by gjwang on 6/5/15.
+//  Copyright (c) 2015 Hexed Bits. All rights reserved.
+//
+
+#import "MessageDataSource.h"
+
+@implementation MessageDataSource
+
+@end
